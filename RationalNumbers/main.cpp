ad3c0d23e4a5763ace0912293c3693616e52ea4c
vinayhpandya <vinayharshad.pandya@sjsu.edu>
2022-02-04 14:16:57 -0800

    Initial Commit

---
+++ RationalNumbers/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RationalNumbers
+//
+//  Created by vinaypandya on 2/4/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
